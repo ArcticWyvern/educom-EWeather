7f4943ad933b14d0901f060ebb75413ea4fcc73d
ArcticWyvern <stan2.hillen@gmail,com>
2023-10-24 16:25:57 +0200

    initial layout of eWeather

---
+++ Models/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EWeather.Models
+{
+    public class Weather
+    {
+        public string Region { get; set; }  
+        public decimal Temperature { get; set; }
+
+        public decimal GroundTemperature { get; set; }  
+
+        public decimal PerceivedTemperature { get; set; }
+
+        public decimal RainfalPastHour { get; set; }
+
+        public decimal Sunpower { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+    }
+}

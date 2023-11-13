068466139ad01f6c41f7b139cb68665fcaaa354c
ArcticWyvern <stan2.hillen@gmail,com>
2023-11-13 13:29:51 +0100

    prep for (#2)

---
+++ EWeatherAPI/EWeatherAPI/Models/StationMeasurement.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EWeatherAPI.Models
+{
+    public class StationMeasurement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }   
+        
+        public string Regio { get; set; }
+
+        public decimal Temperature { get; set; }
+
+        public decimal GroundTemperature { get; set; }
+
+        public decimal FeelTemperature { get; set; }
+
+        public decimal RainFallLastHour { get; set; }
+
+        public decimal Sunpower { get; set; }
+
+        public string WindDirection { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+    }
+}
